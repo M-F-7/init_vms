c89177c34919d0437550af5ebea81b20152458c1
MF7 <160427829+M-F-7@users.noreply.github.com>
2026-02-03 10:20:01 +0100

    add apt update for vscode
    Suppress output when adding the official repository for VS Code.

--- init.sh
+++ init.sh
@@ -228,7 +228,8 @@ if ! check_already_install code; then
         wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/ms_vscode.gpg > /dev/null
         # Ajout du dépôt officiel
         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/ms_vscode.gpg] https://packages.microsoft.com/repos/code stable main" \
-          | sudo tee /etc/apt/sources.list.d/vscode.list
+          | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+        sudo apt update
         install_package code
         code --version
     else
