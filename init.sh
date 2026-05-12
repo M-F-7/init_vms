90ecae71155162388d8d50e02854139798eba458
matteo fernandes <mf7compte@gmail.com>
2026-05-12 19:28:47 +0200

    wip

--- init.sh
+++ init.sh
@@ -656,37 +656,4 @@ fi
 #     echo "NEW_FEATURE installation skipped ❌"
 # fi
 
-# exec zsh
-
-
-
-########################ASSERT################################
-curl -V
-tree --version
-npm -v
-cargo -V
-go version
-pip --version
-pipx --version
-git --version
-ssh -V
-docker --version
-code --version | head -n 1
-zsh --version
-make --version
-opencode --version
-nvim --version | head -n 1
-tmux -V #TOFIX: il se print pas
-# taproom --version
-superfile --version #TOFIX: il se print pas
-branchlet --version
-pkgtop -v
-stormy --version
-smassh --version #TOFIX: il se print pas
-jrnl --version #TOFIX: il se print pas
-# navi --version
-ls ~/.local/bin/edex-ui
-kubectl version --client
-kind version
-
 exec zsh #terminate the script, need to be the last command
